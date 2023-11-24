8dcc44bec41b100a8e3690de98a20b3ff0188c8d
Carlos Castro <carloshenriquemg2020@gmail.com>
2023-11-24 11:36:04 -0300

    first commit

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    // renderiza o componente
+    const { container } = render(<Main />)
+    // busca o elemento e verifica a existencia
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+
+    // gearar snapshot
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    // rederiza o component
+    const { container } = render(<Main />)
+
+    // verifica se o background-color está correto
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
+})
